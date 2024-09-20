169eb822349473c4bd2fb62870471f09fe4fb0eb
Ewenn V <146433402+EwennV@users.noreply.github.com>
2024-09-20 11:15:15 +0200

    Ajout du service UserService #5 (#9)
    * Ajout des méthodes GetAll, Get et Update dans le service UserService #5
    
    * Ajout de la méthode Delete dans le service UserService #5
    
    * Passage des méthodes UserToUserRessource et UserSaveRessourceToUser en public #5

--- StiveBack/Services/UserService.cs
+++ StiveBack/Services/UserService.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿using Microsoft.AspNetCore.Identity;
 using StiveBack.Database;
 using StiveBack.Models;
 using StiveBack.Ressources;
@@ -7,7 +7,8 @@ namespace StiveBack.Services
 {
     public class UserService
     {
-        private MainDbContext _database;
+        private readonly MainDbContext _database;
+        private readonly PasswordHasher<User> _passwordHasher;
 
         public UserService()
         {
@@ -17,6 +18,18 @@ namespace StiveBack.Services
         public UserService(MainDbContext mainDbContext)
         {
             _database = mainDbContext;
+            _passwordHasher = new PasswordHasher<User>();
+        }
+
+        public List<UserRessource> GetAll()
+        {
+            return _database.users.ToArray().Select(user => UserToUserRessource(user)).ToList();
+        }
+
+        public UserRessource Get(int id)
+        {
+            var user = _database.users.FirstOrDefault(user => user.Id == id);
+            return UserToUserRessource(user);
         }
 
         public UserRessource Add(UserSaveRessource userSaveRessource)
@@ -29,7 +42,37 @@ namespace StiveBack.Services
             return UserToUserRessource(user);
         }
 
-        private User UserSaveRessourceToUser(UserSaveRessource userSaveRessource)
+        public UserRessource Update(int id, UserSaveRessource userSaveRessource)
+        {
+            var user = _database.users.FirstOrDefault(user => user.Id == id);
+
+            user.FirstName = userSaveRessource.FirstName;
+            user.LastName = userSaveRessource.LastName;
+            user.Email = userSaveRessource.Email;
+            user.Address1 = userSaveRessource.Address1;
+            user.Address2 = userSaveRessource.Address2;
+            user.PostalCode = userSaveRessource.PostalCode;
+            user.City = userSaveRessource.City;
+            user.Password = _passwordHasher.HashPassword(user, userSaveRessource.Password);
+            user.UserRole = userSaveRessource.RoleIds.Select(roleId => new UserRole
+            {
+                RoleId = roleId
+            }).ToList();
+
+            _database.SaveChanges();
+
+            return UserToUserRessource(user);
+        }
+
+        public void Delete(int id)
+        {
+            var user = _database.users.FirstOrDefault(user => user.Id == id);
+
+            _database.users.Remove(user);
+            _database.SaveChanges();
+        }
+
+        public User UserSaveRessourceToUser(UserSaveRessource userSaveRessource)
         {
             var user = new User
             {
@@ -40,17 +83,18 @@ namespace StiveBack.Services
                 Address2 = userSaveRessource.Address2,
                 PostalCode = userSaveRessource.PostalCode,
                 City = userSaveRessource.City,
-                Password = userSaveRessource.Password,
                 UserRole = userSaveRessource.RoleIds.Select(roleId => new UserRole
                 {
                     RoleId = roleId
                 }).ToList()
             };
 
+            user.Password = _passwordHasher.HashPassword(user, userSaveRessource.Password);
+
             return user;
         }
 
-        private UserRessource UserToUserRessource(User user)
+        public UserRessource UserToUserRessource(User user)
         {
             var userRessource = new UserRessource
             {
