07fc3883fefe16e515f087885cb2270f5407e927
Ewenn V <146433402+EwennV@users.noreply.github.com>
2024-10-16 11:00:11 +0200

    Création de la nouvelle migration #14 (#15)

---
+++ StiveBack/Migrations/20240920131040_Update Product Table.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StiveBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AutoProvisioning",
+                table: "products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SupplierId",
+                table: "products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_CategoryId",
+                table: "products",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_SupplierId",
+                table: "products",
+                column: "SupplierId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products",
+                column: "CategoryId",
+                principalTable: "categories",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_suppliers_SupplierId",
+                table: "products",
+                column: "SupplierId",
+                principalTable: "suppliers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_suppliers_SupplierId",
+                table: "products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_products_CategoryId",
+                table: "products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_products_SupplierId",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "AutoProvisioning",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "SupplierId",
+                table: "products");
+        }
+    }
+}
