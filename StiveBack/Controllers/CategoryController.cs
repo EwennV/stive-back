d776b466ee866aef019e85617a8976d2ecdb02e4
Manuel Tarby <manuel.tarby@gmail.com>
2024-10-18 18:38:43 +0200

    Mise à jour Category #7

---
+++ StiveBack/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StiveBack.Ressources;
+using StiveBack.Services;
+
+namespace StiveBack.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController: ControllerBase
+    {
+        private CategoryService _categoryService { get; set; }
+
+        public CategoryController(CategoryService CategoryService)
+        {
+            _categoryService = CategoryService;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Select(int id)
+        {
+            var category = _categoryService.Select(id);
+            return Ok(category);
+        }
+
+        [HttpGet]
+        public IActionResult SelectAll()
+        {
+            var categories = _categoryService.SelectAll();
+            return Ok(categories);
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(CategoryRessource CategoryRessource)
+        {
+            var category = _categoryService.Add(CategoryRessource);
+            return Ok(category);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory(int CategoryId, CategoryUpdateRessource CategoryRessource)
+        {
+            var category = _categoryService.Update(CategoryId, CategoryRessource);
+            return Ok(category);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCategory(int CategoryRessourceId)
+        {
+            _categoryService.Delete(CategoryRessourceId);
+            return Ok();
+        }
+    }
+}
