0bed71d2248ba8d920a84b07fd81aaf4d4a04a02
EwennV <ewenn.vallois.perso@gmail.com>
2024-12-17 11:10:54 +0100

    Alongement du temps de la session #27

--- StiveBack/Controllers/AuthController.cs
+++ StiveBack/Controllers/AuthController.cs
@@ -17,6 +17,7 @@ namespace StiveBack.Controllers
         private readonly string jwtKey = "MaSuperCleSecreteTresLonguePourJWT12345";
         private readonly MainDbContext _db;
         private readonly PasswordHasher<User> _passwordHasher;
+
         public AuthController(MainDbContext mainDbContext)
         {
             _db = mainDbContext;
@@ -26,18 +27,17 @@ namespace StiveBack.Controllers
         [HttpPost("login")]
         public IActionResult Login([FromBody] UserLogin request)
         {
-            // Exemple utilisateur existant (à remplacer par ton système existant)
             var user = _db.users.FirstOrDefault(u => u.Email == request.Email);
 
+            // Vérifie que l'email fourni correspond à un utilisateur en base de données
             if (null == user) {
-                return Unauthorized(new { message = "Identifiants invalides" });
+                return Unauthorized(new { message = "Identifiants invalides" }); // Erreur floue pour ne pas donner d'indication à un attaquant sur l'origine de l'erreur
             }
 
             if (_passwordHasher.VerifyHashedPassword(user, user.Password, request.Password) == PasswordVerificationResult.Failed)
             {
                 return Unauthorized(new { message = "Identifiants invalides" });
             }
-
 
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.UTF8.GetBytes(jwtKey);
@@ -49,7 +49,7 @@ namespace StiveBack.Controllers
                     new Claim(ClaimTypes.Email, request.Email),
                     new Claim(ClaimTypes.Role, user.IsAdmin == true ? "Admin" : "User")
                 }),
-                Expires = DateTime.UtcNow.AddMinutes(1),
+                Expires = DateTime.UtcNow.AddDays(1), // La session ne durera que 24 heures / 1 jour
                 SigningCredentials = new SigningCredentials(
                     new SymmetricSecurityKey(key),
                     SecurityAlgorithms.HmacSha256Signature)
